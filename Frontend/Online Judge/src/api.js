a091fef1d0f93d4d15d423b29ed4a62ff3fe13c6
Harsh Saw <harshsaw2607@.gmail.com>
2024-06-23 00:47:11 +0530

    Added function to post problem data to backend

--- Frontend/Online Judge/src/api.js
+++ Frontend/Online Judge/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 const URL="http://localhost:3000"
-const uploadData=async (Data)=>{
+const uploadDataRegister=async (Data)=>{
     try{
-        const response=await axios.post(`${URL}/register`,Data)
+        const response=await axios.post(`${URL}/api/auth/register`,Data)
         // console.log("Val ",response)
         return response
     } catch (error){
@@ -20,9 +20,9 @@ const uploadData=async (Data)=>{
     }
 }
 
-const uploadDataLogIn = async() =>{
+ const uploadDataLogIn = async(Data) =>{
     try{
-        const response=await axios.post(`${URL}/login`,Data)
+        const response=await axios.post(`${URL}/api/auth/login`,Data)
         console.log(response.data)
         return response.data
     } catch (error){
@@ -42,4 +42,27 @@ const uploadDataLogIn = async() =>{
     }
 }
 
-export default uploadData
+
+const uploadDataProblem = async (Data) => {
+    try{
+        const response= await axios.post(`${URL}/api/Crud/save`,Data)
+        console.log("newData ",response.data);
+        return response.data
+    } catch (error){
+        if (axios.isAxiosError(error) && error.response) {
+            console.log("Error while creating Coding Problem ", error.response.data);
+            const newResponse={
+                data:{
+                    message:error.response.data,
+                    success:false
+                }
+            }
+            console.log(newResponse)
+            return newResponse; // You can return this to handle the error in the calling function
+        } else {
+            console.log("Error while creating Coding Problem ", error);
+        }
+    }
+}
+
+export {uploadDataRegister,uploadDataLogIn,uploadDataProblem}
