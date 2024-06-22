a634ab48c2c7a1b4073d9c68b68dc8f35ebcee42
Harsh Saw <harshsaw2607@.gmail.com>
2024-06-23 00:48:27 +0530

    Created Routes for different pages

--- Frontend/Online Judge/src/main.jsx
+++ Frontend/Online Judge/src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import SignIn from './Auth/SignIn.jsx'
 import Register from './Auth/Register.jsx'
+import ProblemSetterPage from './Components/Problems/MainPage.jsx'
+import ProblemList from './Components/Problems/ProblemList.jsx'
+import ProblemForm from './Components/Problems/ProblemForm.jsx'
 import { Route, Router, RouterProvider,createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 
 const router=createBrowserRouter(
@@ -11,6 +14,9 @@ const router=createBrowserRouter(
     <>
     <Route path='/login' element = {<SignIn/>}/>
     <Route path='/register' element = {<Register/>}/>
+    <Route path='/main' element = {<ProblemSetterPage/>}/>
+    <Route path='/problemList' element = {<ProblemList/>}/>
+    <Route path='/problemForm' element = {<ProblemForm/>}/>
     </>
   )
 )
