434f10a20171746cf99495894dbc1b5c24bc8baa
Harsh Saw <harshsaw2607@.gmail.com>
2024-07-20 11:07:17 +0530

    Moved backend and compiler URL to environment variable

--- Frontend/Online Judge/src/api.js
+++ Frontend/Online Judge/src/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
-// const URL = "http://15.207.47.162:3000"
-// const URLForCompiler = "http://15.207.47.162:8000"
-const URL = "http://localhost:3000"
-const URLForCompiler = "http://localhost:8000"
+const URL =process.env.VITE_BACKEND_URL
+const URLForCompiler=process.env.VITE_COMPILER_URL
+
 const uploadDataRegister = async (Data) => {
     try {
         const response = await axios.post(`${URL}/api/auth/register`, Data, { withCredentials: true })
