b44a1eb0e10141060e34134ad67d647f55fec3ee
Harsh Saw <harshsaw2607@.gmail.com>
2024-07-21 20:58:53 +0530

    Added media quey for mobile device

--- Frontend/Online Judge/src/Components/Problems/ProblemDetails/Editorial.jsx
+++ Frontend/Online Judge/src/Components/Problems/ProblemDetails/Editorial.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 
 import Header from './Header';
 import CodeEditor from './CodeEditor';
+import useAuth from '../../../Hooks/useAuth';
 
 
 function Editorial() {
@@ -11,7 +12,7 @@ function Editorial() {
   if (state.state.Editorial === null) {
     return <div>Loading Editorial...</div>;
   }
-
+  const { user } = useAuth()
   const Editorial = state.state.Editorial.Editorial
   const id = state.state.id.id
   const problemName = state.state.ProblemName.ProblemName
@@ -20,9 +21,9 @@ function Editorial() {
 
 
   return (
-    <div className='flex flex-row  mx-5 w-[1500px] overflow-x-hidden'>
-      <div className="max-w-3xl  px-4 py-8 bg-white mr-2 max-h-[675px] overflow-auto">
-        <Header id={id} Editorial={Editorial} />
+    <div className='flex flex-col md:flex-row  mx-5 overflow-x-hidden'>
+      <div className="w-full md:max-w-[42rem] md:min-w-[33rem]  px-4 py-8 bg-white mr-2 md:max-h-[675px] overflow-auto">
+        <Header id={id} Editorial={Editorial} ProblemName={problemName} user={user} testcases={testcases} />
         <h1 className="text-2xl font-bold mb-4">{problemName}</h1>
         <pre className=" mb-4 whitespace-pre-wrap">{Editorial}</pre>
       </div>
