3819ee077c7b2123559b7a0dafd61013473651cf
Harsh Saw <harshsaw2607@.gmail.com>
2024-07-22 22:07:48 +0530

    Integrated Vercel Analytics for tracking user interactions on frontend

--- Frontend/Online Judge/src/main.jsx
+++ Frontend/Online Judge/src/main.jsx
@@ -9,32 +9,33 @@ import ProblemDetails from './Components/Problems/ProblemDetails/ProblemDetails.
 import Editorial from './Components/Problems/ProblemDetails/Editorial.jsx'
 import UserContextProvider from './Context/UserContextProvider.jsx'
 import Home from './HomePage/Home.jsx'
-import { Route, Router, RouterProvider,createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Route, Router, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Components/Layout.jsx'
 import MySubmissions from './Components/Problems/ProblemDetails/MySubmissions.jsx'
 import AllSubmissions from './Components/Problems/ProblemDetails/AllSubmissions.jsx'
 import Unauthorised from './Unauthorised.jsx'
 import RequireAuthorisation from './RequireAuthorisation.jsx'
 import ContestPage from './Components/Problems/ContestPage.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
-const router=createBrowserRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-    <Route path='/' element={<Layout/>}>
-    <Route path='problemSet' element = {<ProblemSet/>}/>
-    <Route path='contest' element = {<ContestPage/>}/>
-    <Route path="problemDetails/:id" element={<ProblemDetails/>}/>
-    <Route path='Editorial/:id' element={<Editorial/>}/>
-    <Route path='MySubmission/:id' element={<MySubmissions/>}/>
-    <Route path='AllSubmission/:id' element={<AllSubmissions/>}/>
-    <Route path='' element={<Home/>}/>
-    </Route>
-    <Route path='/' element={<RequireAuthorisation allowedRoles={[1984]}/>}>
-    <Route path='problemForm' element = {<ProblemForm/>}/>
-    </Route>
-    <Route path='login' element = {<SignIn/>}/>
-    <Route path='register' element = {<Register/>}/>
-    <Route path='unauthorised' element={<Unauthorised/>}/>
+      <Route path='/' element={<Layout />}>
+        <Route path='problemSet' element={<ProblemSet />} />
+        <Route path='contest' element={<ContestPage />} />
+        <Route path="problemDetails/:id" element={<ProblemDetails />} />
+        <Route path='Editorial/:id' element={<Editorial />} />
+        <Route path='MySubmission/:id' element={<MySubmissions />} />
+        <Route path='AllSubmission/:id' element={<AllSubmissions />} />
+        <Route path='' element={<Home />} />
+      </Route>
+      <Route path='/' element={<RequireAuthorisation allowedRoles={[1984]} />}>
+        <Route path='problemForm' element={<ProblemForm />} />
+      </Route>
+      <Route path='login' element={<SignIn />} />
+      <Route path='register' element={<Register />} />
+      <Route path='unauthorised' element={<Unauthorised />} />
     </>
   )
 )
@@ -43,11 +44,11 @@ const router=createBrowserRouter(
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserContextProvider>
-    <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </UserContextProvider>
+    <Analytics/>
   </React.StrictMode>,
 )
 
 
-      
-    
+
